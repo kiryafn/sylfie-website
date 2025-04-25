1f9684d49406cb5c7a301fa47648a520bde1ef8e
Kyrylo Alieksieiev <alieksieiev@Sadie-3.local>
2025-04-25 22:48:40 +0200

    Initial commit

---
+++ src/main/java/com/sylfie/SylfieApplication.java
@@ -0,0 +1,13 @@
+package com.sylfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylfieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SylfieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sylfie/SylfieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylfie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SylfieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
