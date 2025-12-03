279b97dcbbc34343f962367017fa047657a51665
Kyrylo Alieksieiev <newkirilal@gmail.com>
2025-12-03 19:50:29 +0100

    Add main application entry point with Spring Boot setup
    Introduced the `SylfieApplication` class as the entry point for the backend using Spring Boot. Enabled scheduling to allow for future scheduled tasks within the application.

---
+++ backend/src/main/java/com/sylfie/SylfieApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.sylfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class SylfieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SylfieApplication.class, args);
+    }
+}
